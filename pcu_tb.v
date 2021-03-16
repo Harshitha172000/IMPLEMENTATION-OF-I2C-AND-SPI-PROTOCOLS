310f0d33ee52712bb36fd3f47c7b74ddb42444bf
Harshitha <59602295+Harshitha172000@users.noreply.github.com>
2021-03-16 11:06:50 +0530

    Update pcu_tb.v

--- pcu_tb.v
+++ pcu_tb.v
@@ -1,31 +1,26 @@
 
 module pcu_tb ();
   
-  parameter SPI_MODE = 3; // CPOL = 0, CPHA = 1
-  parameter SPI_CLK_DELAY = 20;  // 2.5 MHz
-  parameter MAIN_CLK_DELAY = 2;  // 25 MHz
+  parameter SPI_MODE = 3; 
+  parameter SPI_CLK_DELAY = 20;  
+  parameter MAIN_CLK_DELAY = 2;  
 
-  wire w_CPOL; // clock polarity
-  wire w_CPHA; // clock phase
+  wire w_CPOL; 
+  wire w_CPHA; 
 
   assign w_CPOL = (SPI_MODE == 2) | (SPI_MODE == 3);
   assign w_CPHA = (SPI_MODE == 1) | (SPI_MODE == 3);
 
   reg r_Rst_L     = 1'b0;
 
- // reg [7:0] dataPayload[0:255]; 
-  //reg [7:0] dataLength;
-  
-  // CPOL=0, clock idles 0.  CPOL=1, clock idles 1
-//  logic r_SPI_Clk   = w_CPOL ? 1'b1 : 1'b0;
+ 
   wire w_SPI_Clk;
-  //reg r_SPI_En    = 1'b0;
+
   reg r_Clk       = 1'b0;
-  //wire w_SPI_CS_n;
+
   wire w_SPI_MOSI;
   wire w_SPI_MISO;
 
-  // Master Specific
   reg [7:0] r_Master_TX_Byte ;
   reg r_Master_TX_DV = 1'b0;
   reg r_Master_CS_n = 1'b1;
@@ -33,28 +28,24 @@ module pcu_tb ();
   wire r_Master_RX_DV;
   wire [7:0] r_Master_RX_Byte;
 
-  // Slave Specific
   wire       w_Slave_RX_DV; reg r_Slave_TX_DV;
   wire [7:0] w_Slave_RX_Byte; reg [7:0] r_Slave_TX_Byte;
   
   wire i2c_sda, i2c_scl;
   reg reset_i2c=1;
   reg enable=0; 
-  // Clock Generators:
-  always #(MAIN_CLK_DELAY) r_Clk = ~r_Clk;
 
+  always #(MAIN_CLK_DELAY) r_Clk = ~r_Clk;
   // Instantiate UUT
   pcu #(.SPI_MODE(SPI_MODE)) SPI_Slave_UUT
   (
-   // Control/Data Signals,
-   .i_Rst_L(r_Rst_L),      // FPGA Reset
-   .i_Clk(r_Clk),          // FPGA Clock
-   .o_RX_DV(w_Slave_RX_DV),      // Data Valid pulse (1 clock cycle)
-   .o_RX_Byte(w_Slave_RX_Byte),  // Byte received on MOSI
-   .i_TX_DV(r_Slave_TX_DV),      // Data Valid pulse
-   .i_TX_Byte(r_Slave_TX_Byte),  // Byte to serialize to MISO (set up for loopback)
-
-   // SPI Interface
+   
+   .i_Rst_L(r_Rst_L),      
+   .i_Clk(r_Clk),          
+   .o_RX_DV(w_Slave_RX_DV),      
+   .o_RX_Byte(w_Slave_RX_Byte),  
+   .i_TX_DV(r_Slave_TX_DV),      
+   .i_TX_Byte(r_Slave_TX_Byte),  
    .i_SPI_Clk(w_SPI_Clk),
    .o_SPI_MISO(w_SPI_MISO),
    .i_SPI_MOSI(w_SPI_MOSI),
@@ -67,33 +58,32 @@ module pcu_tb ();
    .i2c_scl(i2c_scl)
    );
 
-  // Instantiate Master to drive Slave
+  
   SPI_M
   #(.SPI_MODE(SPI_MODE),
     .CLKS_PER_HALF_BIT(2),
     .NUM_SLAVES(1)) SPI_Master_UUT
   (
-   // Control/Data Signals,
-   .i_Rst_L(r_Rst_L),     // FPGA Reset
-   .i_Clk(r_Clk),         // FPGA Clock
    
-   // TX (MOSI) Signals
-   .i_TX_Byte(r_Master_TX_Byte),     // Byte to transmit on MOSI
-   .i_TX_DV(r_Master_TX_DV),         // Data Valid Pulse with i_TX_Byte
-   .o_TX_Ready(w_Master_TX_Ready),   // Transmit Ready for Byte
+   .i_Rst_L(r_Rst_L),     
+   .i_Clk(r_Clk),         
    
-   // RX (MISO) Signals
-   .o_RX_DV(r_Master_RX_DV),       // Data Valid pulse (1 clock cycle)
-   .o_RX_Byte(r_Master_RX_Byte),   // Byte received on MISO
+   
+   .i_TX_Byte(r_Master_TX_Byte),     
+   .i_TX_DV(r_Master_TX_DV),         
+   .o_TX_Ready(w_Master_TX_Ready),   
+   
+   
+   .o_RX_DV(r_Master_RX_DV),    
+   .o_RX_Byte(r_Master_RX_Byte),   
 
-   // SPI Interface
+   
    .o_SPI_Clk(w_SPI_Clk),
    .i_SPI_MISO(w_SPI_MISO),
    .o_SPI_MOSI(w_SPI_MOSI)
    );
 
 
-  // Sends a single byte from master to slave.  Will drive CS on its own.
   task SendSingleByte(input [7:0] data);begin
     @(posedge r_Clk);
     
@@ -106,7 +96,7 @@ module pcu_tb ();
     r_Master_CS_n    <= 1'b1;  
       
     end
-  endtask // SendSingleByte
+  endtask 
 
 
 
@@ -147,6 +137,6 @@ module pcu_tb ();
      
      
       $finish();      
-    end // initial begin
+    end 
 
-endmodule // SPI_Slave
+endmodule 
